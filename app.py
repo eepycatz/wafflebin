47911fa12205a863336f9f5ba24b839ef1a80cf2
eepycatz <241014658+eepycatz@users.noreply.github.com>
2026-06-07 00:21:24 +0000

    Fix Render port binding

--- app.py
+++ app.py
@@ -103,5 +103,10 @@ def nice_date_filter(value):
     return dt.strftime("%Y-%m-%d %H:%M UTC")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
